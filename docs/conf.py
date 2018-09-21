c8404b709b75ed0a3fc550cb2d969c120d7a5324
Romain Dartigues <romain-dartigues@users.noreply.github.com>
2018-09-21 21:49:06 +0200

    docs: add logo

--- docs/conf.py
+++ docs/conf.py
@@ -90,6 +90,8 @@ pygments_style = None
 #
 html_theme = 'alabaster'
 
+html_logo = '_static/logo.svg'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
